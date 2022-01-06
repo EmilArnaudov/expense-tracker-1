5c878b128df39e5fada54381a81512f65e4f6427
EmilArnaudov <emil.arnaudov.ecom@gmail.com>
2022-01-06 20:04:20 +0000

    Removed handlebars / installed express-handlebars

--- src/config/express.js
+++ src/config/express.js
@@ -1,5 +1,16 @@
-const app = require('express');
+const express = require('express');
+const handlebars = require('express-handlebars');
 
+// Init application
+const app = express();
+
+//Set up view engine
+app.engine('hbs', handlebars({
+    defaultLayout: 'main',
+    extname: '.hbs',
+}));
+
+app.set('view engine', 'hbs')
 
 
 module.exports = app;
