cf7e16fbe7f1ab21cc222c5bd55099e1f54e4931
EmilArnaudov <emil.arnaudov.ecom@gmail.com>
2022-01-07 18:05:07 +0000

    created basic register page

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,11 @@
 const routes = require('express').Router();
 const loginController = require('../controllers/loginController');
+const registerController = require('../controllers/registerController');
 
 routes.get('/', (req, res) => {
     res.redirect('/login');
 })
 routes.use('/login', loginController);
+routes.use('/register', registerController);
 
 module.exports = routes;
