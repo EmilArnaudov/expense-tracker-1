717465438af81f56eec73dfa47b0b7f1f70fd50d
EmilArnaudov <emil.arnaudov.ecom@gmail.com>
2022-01-08 10:36:41 +0000

    created User model

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        validate: [/^[a-zA-Z]\w+$/, 'Username can contain only alphanumeric characters ([A-Z] [a-z] [0-9] and \'_\')'],
+        minlength: [6, 'Username must be between 6 - 18 characters.'],
+        maxlength: [18, 'Username must be between 6 - 18 characters.'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: [/^[a-zA-Z]\w+$/, 'Password can contain only alphanumeric characters ([A-Z] [a-z] [0-9] and \'_\')'],
+        minlength: [8, 'Password must be between 8 - 30 characters.'],
+        maxlength: [30, 'Password must be between 8 - 30 characters.']
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
