a4f1a1cac2d73738788f0552bc549bf5e78e4b73
EmilArnaudov <emil.arnaudov.ecom@gmail.com>
2022-01-08 14:51:12 +0000

    installed jsonwebtoken and added home route

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,9 @@
 const routes = require('express').Router();
 const loginController = require('../controllers/authControllers/loginController');
 const registerController = require('../controllers/authControllers/registerController');
+const homeController = require('../controllers/homeController')
 
-routes.get('/', (req, res) => {
-    res.redirect('/login');
-})
+routes.use('/', homeController)
 routes.use('/login', loginController);
 routes.use('/register', registerController);
 
