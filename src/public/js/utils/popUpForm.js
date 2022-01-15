50b8427088310e7c6835d6b30d35bdbdfe1f26a0
EmilArnaudov <emil.arnaudov.ecom@gmail.com>
2022-01-15 08:07:43 +0000

    test modal form

---
+++ src/public/js/utils/popUpForm.js
@@ -0,0 +1,7 @@
+// const form = document.querySelector('#transaction-form');
+
+// form.addEventListener('change', test)
+
+// function test() {
+//     form.querySelectorAll('select').forEach(x => x.disabled = true);
+// }
