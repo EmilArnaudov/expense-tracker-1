8cab90f159a6823748d28be9c8cfa81c438f4781
EmilArnaudov <emil.arnaudov.ecom@gmail.com>
2022-01-16 17:22:51 +0000

    add currency formatter

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,23 +2,28 @@ const router = require('express').Router();
 const User = require('../models/User');
 const { splitTransactionData }= require('../utils/transactionUtils')
 const { addTransaction } = require('../services/transactionService');
+const {formatter} = require('../utils/currencyFormatter')
 
 router.get('/', (req, res) => {
     User.findById(req.user._id)
         .lean()
         .then(user => {
+            user.balance = formatter.format(user.balance)
             res.render('home', {user})
+            
         });
 })
 
 router.post('/', async (req, res) => {
     let { type, category, expense, date, amount } = splitTransactionData(req.body);
     let user = await User.findById(req.user._id).lean();
-    
-    let result = await addTransaction(user._id, type, category, expense, date, amount);
+    user.balance = formatter.format(user.balance)
 
-    if (result) {
-        res.render('home', {user, error: result})
+
+    let error = await addTransaction(user._id, type, category, expense, date, amount);
+
+    if (error) {
+        res.render('home', {user, error})
         return;
     }
 
