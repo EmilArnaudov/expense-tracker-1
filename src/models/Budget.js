dee0b673528042bd719e41428e9cddb318f6ad19
EmilArnaudov <emil.arnaudov.ecom@gmail.com>
2022-01-16 20:09:14 +0000

    added fields to budget model

--- src/models/Budget.js
+++ src/models/Budget.js
@@ -6,18 +6,47 @@ const budgetSchema = new mongoose.Schema({
         ref: 'User',
         required: true,
     },
-
-    title: {
+    category: {
         type: String,
+        enum: ['housing', 'transportation', 'food', 'utilities', 'clothing', 'healthcare', 'insurance', 'household-items', 'personal', 'debt', 'savings', 'education', 'gifts', 'entertainment'],
         required: true,
     },
 
     maxValue: {
         type: Number,
         min: [1, 'Budget max value cannot be less than 1'],
+        required: true,
     },
 
-    currentValue: 0,
+    currentValue: {
+        type: Number,
+        min: 0,
+        max: 0,
+    },
+
+    percentageFilled: {
+        type: Number,
+    },
+
+    title: {
+        type:String
+    }
+})
+
+budgetSchema.pre('save', function() {
+    this.percentageFilled = Math.round(this.currentValue / this.maxValue) * 100;
+})
+
+budgetSchema.pre('save', function() {
+    let title;
+    if (this.category === 'household-items') {
+        title = 'Household Items';
+    } else {
+        title = this.category[0].toUpperCase() + this.category.slice(1);
+    }
+
+    this.title = title;
+    
 })
 
 const Budget = mongoose.model('Budget', budgetSchema);
